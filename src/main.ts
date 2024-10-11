18a7f0aeb8f3dbca1a1e3af6eeb0187472f60d92
nenahui <kanatraccoon@gmail.com>
2024-10-11 13:25:29 +0600

    made a launch on localhost

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000, '192.168.8.185');
+  await app.listen(3000);
 }
 bootstrap();
