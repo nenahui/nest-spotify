71bdb6b147762fdc72ab2a1fcdd30cf86dca4ee1
nenahui <kanatraccoon@gmail.com>
2024-10-11 12:16:36 +0600

    configured saving images in packs and keeping the original extension

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -8,9 +9,13 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { randomUUID } from 'crypto';
 import type { Model } from 'mongoose';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { Albums, type AlbumDocument } from '../schemas/albums.schema';
 import type { CreateAlbumDto } from './create-album.dto';
+import { existsSync, mkdirSync } from 'fs';
 
 @Controller('albums')
 export class AlbumsController {
@@ -24,16 +29,58 @@ export class AlbumsController {
   }
 
   @Post()
-  @UseInterceptors(FileInterceptor('image', { dest: './public/images' }))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: (_req, _file, callback) => {
+          const uploadPath = './public/images/albums';
+
+          if (!existsSync(uploadPath)) {
+            mkdirSync(uploadPath, { recursive: true });
+          }
+
+          callback(null, uploadPath);
+        },
+        filename: (_req, file, callback) => {
+          const uniqueName = randomUUID();
+          const ext = extname(file.originalname);
+          callback(null, uniqueName + ext);
+        },
+      }),
+      fileFilter: (_req, file, callback) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+          callback(null, true);
+        } else {
+          callback(
+            new BadRequestException(
+              'Only JPG, JPEG, and PNG files are allowed',
+            ),
+            false,
+          );
+        }
+      },
+      limits: { fileSize: 1024 * 1024 * 5 },
+    }),
+  )
   async createAlbum(
     @Body() albumDto: CreateAlbumDto,
     @UploadedFile() file: Express.Multer.File,
   ) {
+    const { artist, name, release } = albumDto;
+
+    if (!artist) {
+      throw new BadRequestException('Artist is required');
+    } else if (!name) {
+      throw new BadRequestException('Name is required');
+    } else if (!release) {
+      throw new BadRequestException('Release is required');
+    }
+
     return await this.albumModel.create({
-      artist: albumDto.artist,
-      name: albumDto.name,
-      release: albumDto.release,
-      image: file ? file.filename : null,
+      artist,
+      name,
+      release,
+      image: file ? `images/albums/${file.filename}` : null,
     });
   }
 }
--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -9,9 +10,13 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { FileInterceptor } from '@nestjs/platform-express';
 import type { Express } from 'express';
+import { existsSync, mkdirSync } from 'fs';
 import type { Model } from 'mongoose';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { Artists, type ArtistsDocument } from '../schemas/artists.schema';
 import type { CreateArtistDto } from './create-artist.dto';
+import { randomUUID } from 'crypto';
 
 @Controller('artists')
 export class ArtistsController {
@@ -25,15 +30,53 @@ export class ArtistsController {
   }
 
   @Post()
-  @UseInterceptors(FileInterceptor('image', { dest: './public/images' }))
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: (_req, _file, callback) => {
+          const uploadPath = './public/images/artists';
+
+          if (!existsSync(uploadPath)) {
+            mkdirSync(uploadPath, { recursive: true });
+          }
+
+          callback(null, uploadPath);
+        },
+        filename: (_req, file, callback) => {
+          const uniqueName = randomUUID();
+          const ext = extname(file.originalname);
+          callback(null, uniqueName + ext);
+        },
+      }),
+      fileFilter: (_req, file, callback) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+          callback(null, true);
+        } else {
+          callback(
+            new BadRequestException(
+              'Only JPG, JPEG, and PNG files are allowed',
+            ),
+            false,
+          );
+        }
+      },
+      limits: { fileSize: 1024 * 1024 * 5 },
+    }),
+  )
   async createArtist(
     @Body() artistsDto: CreateArtistDto,
     @UploadedFile() file: Express.Multer.File,
   ) {
+    const { name, information } = artistsDto;
+
+    if (!name) {
+      throw new BadRequestException('Name is required');
+    }
+
     return await this.artistModel.create({
-      name: artistsDto.name,
-      information: artistsDto.information,
-      image: file ? file.filename : null,
+      name,
+      information,
+      image: file ? `images/artists/${file.filename}` : null,
     });
   }
 }
